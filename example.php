210d035494de31c7490e65159b83bb18474f38ca
NovikovRoman <service.for.roman@gmail.com>
2017-03-06 22:24:55 +0300

    исправлены комментарии и параметр

--- example.php
+++ example.php
@@ -3,12 +3,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Pagination\Templates\Hellip;
 use Pagination\Templates\Simple;
 
-$currentPage = 9;
+$currentPage = 7;
 $total = 200;
 $url = '/news/articles/2017/';
 
 /* template Hellip ------------------------------------------------------------------- */
-/* 1 … 5 6 <7> 8 9 … 21 */
+/* 1 … 5 6 <7> 8 9 … 20 */
 $pagen = new Hellip($url);
 $pagen->setTotal($total)->setCurrentPage($currentPage);
 // $pagen->setPageSize(10); // the default
@@ -17,7 +17,7 @@ echo $pagen->getHtml();
 echo '<hr>';
 
 /* template Simple ------------------------------------------------------------------- */
-/* «  2 3 4 5 6  » */
+/* «  5 6 <7> 8 9  » */
 $pagen = new Simple($url);
 $pagen->setTotal($total)->setCurrentPage($currentPage);
 // $pagen->setPageSize(10); // the default
